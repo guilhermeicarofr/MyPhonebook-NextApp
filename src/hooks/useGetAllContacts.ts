2fe9bb3d9542bd769aefdbc8f1b5543a674dd2ee
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-05-01 15:26:16 -0300

    feat: created search by name feature on contacts list using name input

--- src/hooks/useGetAllContacts.ts
+++ src/hooks/useGetAllContacts.ts
@@ -1,12 +1,17 @@
 import { ContactsList } from '@/models/contactsModels';
 import { useGetLocalStorage } from './useGetLocalStorage';
 
-function useGetAllContacts(user: string) {
+function useGetAllContacts(user: string, search: string) {
   const table = useGetLocalStorage(user);
-
-  const list = [] as ContactsList;
+  let list = [] as ContactsList;
   for(const id in table) {
     list.push(table[id]);
+  }
+
+  if(search) {
+    list = list.filter((contact) => {
+      return (search === contact.name.substring(0, (search.length)));
+    });
   }
   
   return list;
